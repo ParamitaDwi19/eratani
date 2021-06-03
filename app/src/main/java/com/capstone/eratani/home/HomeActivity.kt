1ca71ff2da22815681420fed0b5a2e762da1dce4
Suryasa Made <suryasa.made@gmail.com>
2021-06-04 04:01:45 +0800

    Just work enough

--- app/src/main/java/com/capstone/eratani/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/eratani/home/HomeActivity.kt
@@ -2,17 +2,19 @@ package com.capstone.eratani.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.eratani.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.capstone.eratani.profile.ProfileFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_home)
 
         val navView: BottomNavigationView = findViewById(R.id.bot_nav)
@@ -20,7 +22,7 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_home, R.id.navigation_product, R.id.navigation_profile
+            R.id.navigation_home, R.id.navigation_profile
         ).build()
 
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/capstone/eratani/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstone/eratani/profile/ProfileFragment.kt
@@ -1,60 +1,67 @@
 package com.capstone.eratani.profile
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.capstone.eratani.LoginActivity
 import com.capstone.eratani.R
+import com.capstone.eratani.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var fragmentProfileBinding: FragmentProfileBinding
+    private val user = FirebaseAuth.getInstance()
+    private val TAG = ProfileFragment::class.java.simpleName
+
+    companion object {
+        val EXTRA_STATUS = "extra_status"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentProfileBinding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return fragmentProfileBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val status = arguments?.getString(EXTRA_STATUS)
+//        Log.d(TAG, "haha: $status")
+        var email: String? = null
+
+        user.currentUser?.let {
+            for (profile in it.providerData) {
+                email = profile.email
             }
+
+            fragmentProfileBinding.tvEmail.text = email
+//            fragmentProfileBinding.tvStatus.text = status
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.logout_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                user.signOut()
+                val intent = Intent(requireActivity(), LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
